d90af5beefd3129fc539a387bea52e76c02226b6
Panshul Gupta <panshul007@gmail.com>
2015-10-18 19:18:29 +0200

    custom middleware

--- node-app/app.js
+++ node-app/app.js
@@ -8,6 +8,12 @@ var port = process.env.PORT || 3000; // process.env gets the environment variabl
 
 app.use('/assets', express.static(__dirname + '/public')); // middle ware to stream the static files from public folder to the response.
 
+//Creating ur own middleware. Very powerful to reuse the requsts and response. u can also use other third party middle wares as well.
+app.use('/', function(req, res, next) {
+	console.log('Request Url: ' + req.url);
+	next(); // calls the next middleware function.
+});
+
 app.get('/', function(req, res) {
 	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h1>Hello World Express</h1></body></html>');
 });
