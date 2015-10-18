01fc34876ba7b32c0b3535da112d0401d1fda5ad
Panshul Gupta <panshul007@gmail.com>
2015-10-18 18:54:10 +0200

    express routing

--- node-app/app.js
+++ node-app/app.js
@@ -1,2 +1,17 @@
 var moment = require('moment');
 console.log(moment().format("ddd, hA"));
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000; // process.env gets the environment variable.
+
+app.get('/', function(req, res) {
+	res.send('<html><head></head><body><h1>Hello World Express</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John', lastname: 'Doe'});
+});
+
+app.listen(port);
