daab875fd0ca02dbb1fe67eaa32ab4c9b233b2b3
Panshul Gupta <panshul007@gmail.com>
2015-10-18 19:38:53 +0200

    ejs templating

--- node-app/app.js
+++ node-app/app.js
@@ -6,6 +6,8 @@ var app = express();
 
 var port = process.env.PORT || 3000; // process.env gets the environment variable.
 
+app.set('view engine', 'ejs'); // set the template engine.
+
 app.use('/assets', express.static(__dirname + '/public')); // middle ware to stream the static files from public folder to the response.
 
 //Creating ur own middleware. Very powerful to reuse the requsts and response. u can also use other third party middle wares as well.
@@ -15,12 +17,20 @@ app.use('/', function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h1>Hello World Express</h1></body></html>');
+	res.render('index');
 });
 
+// app.get('/', function(req, res) {
+// 	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h1>Hello World Express</h1></body></html>');
+// });
+
+// app.get('/person/:id', function(req, res) {
+// 	res.send('<html><head></head><body><h1>Person: ' + 
+// 	req.params.id + '</h1></body></html>');
+// });
+
 app.get('/person/:id', function(req, res) {
-	res.send('<html><head></head><body><h1>Person: ' + 
-	req.params.id + '</h1></body></html>');
+	res.render('person', { ID: req.params.id });
 });
 
 app.get('/api', function(req, res) {
