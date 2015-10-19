91aae8ce57a4e73479d8c980199ae995f4e27ed4
Panshul Gupta <panshul007@gmail.com>
2015-10-19 23:03:09 +0200

    query string

--- node-app/app.js
+++ node-app/app.js
@@ -33,6 +33,11 @@ app.get('/person/:id', function(req, res) {
 	res.render('person', { ID: req.params.id });
 });
 
+
+app.get('/personqstr/:id', function(req, res) {
+	res.render('personqstr', { ID: req.params.id, QSTR: req.query.qstr });
+});
+
 app.get('/api', function(req, res) {
 	res.json({ firstname: 'John', lastname: 'Doe'});
 });
